c28829ced001a02124856b6b5541ca96bb6abca3
SenaOzturkk <sena.ozturk@appelier.net>
2025-03-20 17:23:19 +1100

    init comment

---
+++ src/main/java/com/askallbackend/askall/AskAllApplication.java
@@ -0,0 +1,13 @@
+package com.askallbackend.askall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AskAllApplication.class, args);
+    }
+
+}
