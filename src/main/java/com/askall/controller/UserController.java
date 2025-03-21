37348d9e00afb771235ced940dd81c2ed9f40f1f
SenaOzturkk <sena.ozturk@appelier.net>
2025-03-21 17:12:19 +1100

    user api başlandı 2 tane

---
+++ src/main/java/com/askall/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.askall.controller;
+
+import com.askall.modal.User;
+import com.askall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userService.createUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+}
---
+++ src/main/java/com/askall/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.askall.repository;
+
+import com.askall.modal.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
